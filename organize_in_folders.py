6b820228afbe7751cfc9a80602e37e5cece18b53
Gerard Sanroma <gerard.sanroma-gueell@dzne.de>
2019-06-05 15:35:54 +0200

    added option of relative path by default

--- organize_in_folders.py
+++ organize_in_folders.py
@@ -7,6 +7,7 @@ parser.add_argument("--in_dir", type=str, nargs=1, required=True, help="director
 parser.add_argument("--in_suffix_list", type=str, nargs='+', required=True, help="list of suffixes for each modality")
 parser.add_argument("--out_dir", type=str, nargs=1, required=True, help="base directory where to create the out directory structure")
 parser.add_argument("--out_name_list", type=str, nargs='+', help="(optional) list of names to give to output files (if not given, same as original names)")
+parser.add_argument("--abspath", type='store_true', help="whether to use absolute path for symlinks (default: relative from \'in_dir\')")
 
 args = parser.parse_args()
 
@@ -27,12 +28,19 @@ os.makedirs(args.out_dir[0])
 for name in names_list:
     os.makedirs(os.path.join(args.out_dir[0], name))
 
+# change to output directory (in case not absolute paths)
+if not args.abspath:
+    os.chdir(args.out_dir[0])
+
 # link each subject files to the output subject subdirectory
 for name, files_list in zip(names_list, files_superlist_t):
     subject_dir = os.path.join(args.out_dir[0], name)
     for i, file in enumerate(files_list):
+        src = os.path.join(args.in_dir[0], file)
+        if not args.abspath:
+            src = os.path.relpath(os.path.join(args.in_dir[0], file))
         if args.out_name_list is not None:
-            os.symlink(os.path.join(args.in_dir[0], file), os.path.join(subject_dir, args.out_name_list[i]))
+            os.symlink(src, os.path.join(subject_dir, args.out_name_list[i]))
         else:
-            os.symlink(os.path.join(args.in_dir[0], file), os.path.join(subject_dir, file))
+            os.symlink(src, os.path.join(subject_dir, file))
 
